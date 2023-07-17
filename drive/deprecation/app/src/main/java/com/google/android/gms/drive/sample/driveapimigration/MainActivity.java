017429a89e96d5adb5ffa8f2bf17cd139224f238
Darush <darushfaqiri@gmail.com>
2023-07-17 17:01:56 +0300

    build: migrate to AndroidX, upgrade gradle plugin to 7.4.2, upgrade the library dependency versions

--- drive/deprecation/app/src/main/java/com/google/android/gms/drive/sample/driveapimigration/MainActivity.java
+++ drive/deprecation/app/src/main/java/com/google/android/gms/drive/sample/driveapimigration/MainActivity.java
@@ -29,7 +29,7 @@ import com.google.api.services.drive.model.File;
 import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.EditText;
